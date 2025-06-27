1d479341afb9dc0277e0fe440c9cc4ad2cf5b00b
OzuAkira <22311038ao@tama.ac.jp>
2025-06-27 16:08:55 +0900

    斬撃　試行錯誤

--- Assets/script/Cutter.cs
+++ Assets/script/Cutter.cs
@@ -1,17 +1,24 @@
-using EzySlice;
+using EzySlice;// References https://github.com/DavidArayan/ezy-slice
 using UnityEngine;
 
-public class Cutter : MonoBehaviour
+public class cutter : MonoBehaviour
 {
     public Transform plane;
     public Material crossMaterial;
     public Camera cam;
 
+   /* public Transform cam_transform;
+    private void Start()
+    {
+        cam_transform = cam.transform;
+        cam_transform.rotation = Quaternion.Euler(15, 0, 0);
+    }
+   */
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SlicedHull hull = gameObject.Slice(plane.position, cam.transform.up);
+            SlicedHull hull = gameObject.Slice(plane.position+new Vector3(0,0.1f,0), cam.transform.up);
 
             if (hull != null)
             {
@@ -22,7 +29,31 @@ public class Cutter : MonoBehaviour
 
                 upperHull.AddComponent<MeshCollider>().convex = true;
                 lowerHull.AddComponent<MeshCollider>().convex = true;
+
+                plane = lowerHull.GetComponent<Transform>();
+//                cam_transform.rotation = Quaternion.Euler(-15, 0, 0);
+                SlicedHull Hull_2 = lowerHull.Slice(plane.position + new Vector3(0,-0.1f,0),cam.transform.up);
+
+
+                //Destroy(lowerHull);
+                
+                if (Hull_2 != null)
+                {
+                    Debug.Log("run");
+                    //GameObject upperHull_2 = Hull_2.CreateUpperHull(lowerHull, crossMaterial);
+                    GameObject lowerHull_2 = Hull_2.CreateLowerHull(lowerHull, crossMaterial);
+
+                    Destroy(lowerHull);
+
+                    //upperHull_2.AddComponent<MeshCollider>().convex = true;
+                    lowerHull_2.AddComponent<MeshCollider>().convex = true;
+                }
+
+                    /*
+                    upperHull.transform.position += new Vector3(-1, 0, 0);
+                    lowerHull.transform.position += new Vector3(1, 0, 0);
+                    */
+                }
             }
-        }
     }
 }
